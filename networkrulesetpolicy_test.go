db22619a5afe8ea509fe100e0c45c8b787ab4545
Mohammad Alkhamis <malkhamis@paloaltonetworks.com>
2021-07-27 15:54:18 -0700

    new: unit test for custom validators on netrulesetpolicy

---
+++ networkrulesetpolicy_test.go
@@ -0,0 +1,120 @@
+package gaia
+
+import (
+	"testing"
+
+	"go.aporeto.io/elemental"
+)
+
+func TestNetworkRuleSetPolicy_Validate_custom(t *testing.T) {
+
+	cases := map[string]struct {
+		p   *NetworkRuleSetPolicy
+		err elemental.Error
+	}{
+
+		"valid": {
+			p: &NetworkRuleSetPolicy{
+				Name: "dummy",
+				Subject: [][]string{
+					{"tag=valid"},
+				},
+			},
+		},
+
+		"empty-subject": {
+			p: &NetworkRuleSetPolicy{
+				Name:    "dummy",
+				Subject: nil,
+			},
+			err: makeValidationError("subject", "Expression must contain at least one sub-expression"),
+		},
+
+		"empty-subexpression-in-subject": {
+			p: &NetworkRuleSetPolicy{
+				Name:    "dummy",
+				Subject: [][]string{{}},
+			},
+			err: makeValidationError("subject", "Sub-expression must not be empty"),
+		},
+
+		"duplicate-tags-in-subject": {
+			p: &NetworkRuleSetPolicy{
+				Name: "dummy",
+				Subject: [][]string{
+					{"tag=dup", "tag=dup"},
+				},
+			},
+			err: makeValidationError("subject", "Duplicate tag in a sub-expression: 'tag=dup'"),
+		},
+
+		"duplicate-subexpressions-in-subject": {
+			p: &NetworkRuleSetPolicy{
+				Name: "dummy",
+				Subject: [][]string{
+					{"tag1=a", "tag2=b"},
+					{"tag2=b", "tag1=a"},
+				},
+			},
+			err: makeValidationError("subject", "Duplicate equivalent sub-expressions found"),
+		},
+
+		"bad-tag-in-subject": {
+			p: &NetworkRuleSetPolicy{
+				Name:    "dummy",
+				Subject: [][]string{{"troubles"}},
+			},
+			err: makeValidationError("subject", "'troubles' must contain at least one '=' symbol separating two valid words"),
+		},
+
+		"duplicate-incoming-rules": {
+			p: &NetworkRuleSetPolicy{
+				Name: "dummy",
+				Subject: [][]string{
+					{"tag=valid"},
+				},
+				IncomingRules: []*NetworkRule{
+					{
+						Action:        NetworkRuleActionAllow,
+						Object:        [][]string{{"tag1=a", "tag2=b"}},
+						ProtocolPorts: []string{"udp/123", "tcp/80"},
+					},
+					{
+						Action:        NetworkRuleActionAllow,
+						Object:        [][]string{{"tag2=b", "tag1=a"}},
+						ProtocolPorts: []string{"tcp/80", "udp/123"},
+					},
+				},
+			},
+			err: makeValidationError("incomingRules", "Duplicate network rules at the following indexes: [1, 2]"),
+		},
+
+		"duplicate-outgoing-rules": {
+			p: &NetworkRuleSetPolicy{
+				Name: "dummy",
+				Subject: [][]string{
+					{"tag=valid"},
+				},
+				OutgoingRules: []*NetworkRule{
+					{
+						Action:        NetworkRuleActionAllow,
+						Object:        [][]string{{"tag1=a", "tag2=b"}},
+						ProtocolPorts: []string{"udp/123", "tcp/80"},
+					},
+					{
+						Action:        NetworkRuleActionAllow,
+						Object:        [][]string{{"tag2=b", "tag1=a"}},
+						ProtocolPorts: []string{"tcp/80", "udp/123"},
+					},
+				},
+			},
+			err: makeValidationError("outgoingRules", "Duplicate network rules at the following indexes: [1, 2]"),
+		},
+	}
+
+	for name, c := range cases {
+		t.Run(name, func(t *testing.T) {
+			checkValidationReturnsExpectedErr(c.p, c.err, t)
+		})
+	}
+}
