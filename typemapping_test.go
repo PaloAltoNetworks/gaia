1689e32c64fd38de748f0456001022261888b651
Mohammad Alkhamis <malkhamis@paloaltonetworks.com>
2021-10-07 11:19:33 -0700

    edit: make the test more specific

--- typemapping_test.go
+++ typemapping_test.go
@@ -15,15 +15,17 @@ func TestTypeMapping_all_have_key_openapi3(t *testing.T) {
 	}
 
 	var decodedTypeMapping map[string]struct {
-		OpenAPI3 interface{} `yaml:"openapi3,omitempty"`
+		OpenAPI3 struct {
+			Type interface{} `yaml:"type,omitempty"`
+		} `yaml:"openapi3,omitempty"`
 	}
 	if err := yaml.NewDecoder(file).Decode(&decodedTypeMapping); err != nil {
 		t.Fatalf("error decoding the content of _type.mapping file: %v", err)
 	}
 
 	for typeName, mapping := range decodedTypeMapping {
-		if mapping.OpenAPI3 == nil {
-			t.Errorf("type '%s' has no mapping defined for 'openapi3'", typeName)
+		if mapping.OpenAPI3.Type == nil {
+			t.Errorf("'%s' has no type mapping defined for 'openapi3'", typeName)
 		}
 	}
 }
