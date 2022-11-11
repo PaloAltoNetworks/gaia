d62238eee7279699334d13306adfad63f3707f1e
Jinren Ping <30264958+jping0929@users.noreply.github.com>
2022-11-10 17:15:51 -0800

    New: gcp resource processor (#856)
    * New: gcp resource processor
    
    * Fix: spelling
    
    * Update: gcp resource example value
    
    * Update: make gcpresource private
    
    * Fix: spelling

--- relationships_registry.go
+++ relationships_registry.go
@@ -2543,7 +2543,58 @@ func init() {
 		},
 	}
 
-	relationshipsRegistry[GCPResourceIdentity] = &elemental.Relationship{}
+	relationshipsRegistry[GCPAssetIdentity] = &elemental.Relationship{
+		RetrieveMany: map[string]*elemental.RelationshipInfo{
+			"root": {
+				Parameters: []elemental.ParameterDefinition{
+					{
+						Name:     "q",
+						Type:     "string",
+						Multiple: true,
+					},
+				},
+			},
+		},
+		Info: map[string]*elemental.RelationshipInfo{
+			"root": {
+				Parameters: []elemental.ParameterDefinition{
+					{
+						Name:     "q",
+						Type:     "string",
+						Multiple: true,
+					},
+				},
+			},
+		},
+	}
+
+	relationshipsRegistry[GCPResourceIdentity] = &elemental.Relationship{
+		Create: map[string]*elemental.RelationshipInfo{
+			"root": {},
+		},
+		RetrieveMany: map[string]*elemental.RelationshipInfo{
+			"root": {
+				Parameters: []elemental.ParameterDefinition{
+					{
+						Name:     "q",
+						Type:     "string",
+						Multiple: true,
+					},
+				},
+			},
+		},
+		Info: map[string]*elemental.RelationshipInfo{
+			"root": {
+				Parameters: []elemental.ParameterDefinition{
+					{
+						Name:     "q",
+						Type:     "string",
+						Multiple: true,
+					},
+				},
+			},
+		},
+	}
 
 	relationshipsRegistry[GraphEdgeIdentity] = &elemental.Relationship{
 		RetrieveMany: map[string]*elemental.RelationshipInfo{
--- specs/root.spec
+++ specs/root.spec
@@ -574,6 +574,20 @@ relations:
   create:
     description: Create a flow statistics report.
 
+- rest_name: gcpasset
+  get:
+    description: Retrieves the list of stored gcp resources.
+    global_parameters:
+    - $filtering
+
+- rest_name: gcpresource
+  get:
+    description: Retrieves the list of stored gcp resources.
+    global_parameters:
+    - $filtering
+  create:
+    description: creates a new gcp resource from field 'data'.
+
 - rest_name: graphedge
   get:
     description: Retrieves the graph edges.
